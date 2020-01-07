33b3f176799e5f724f662e8c1d03a13214ca89db
hlxnd <hlxnd@h9d.eu>
2020-01-07 14:06:20 +0100

    Add DB routines to ContactsApiController

--- spring-server/src/main/java/fr/hildenbrand/contactapi/api/ContactsApiController.java
+++ spring-server/src/main/java/fr/hildenbrand/contactapi/api/ContactsApiController.java
@@ -1,11 +1,13 @@
 package fr.hildenbrand.contactapi.api;
 
+import fr.hildenbrand.contactapi.dbaccess.ContactRepository;
 import fr.hildenbrand.contactapi.model.Contact;
 import fr.hildenbrand.contactapi.model.Skill;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -26,6 +28,9 @@ import java.util.List;
 @Controller
 public class ContactsApiController implements ContactsApi {
 
+	@Autowired
+	ContactRepository contactRepository;
+	
     private static final Logger log = LoggerFactory.getLogger(ContactsApiController.class);
 
     private final ObjectMapper objectMapper;
@@ -40,7 +45,15 @@ public class ContactsApiController implements ContactsApi {
 
     public ResponseEntity<Void> addContact(@ApiParam(value = "Contact object that needs to be added" ,required=true )  @Valid @RequestBody Contact body) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        try {
+        	// Reset ID
+        	body.setId(null);
+        	contactRepository.save(body);
+        	return new ResponseEntity<Void>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<Void> addSkillToContact(@ApiParam(value = "ID of contact to update",required=true) @PathVariable("contactId") Long contactId,@ApiParam(value = "ID of skill to add" ,required=true )  @Valid @RequestBody Skill skills) {
@@ -49,41 +62,45 @@ public class ContactsApiController implements ContactsApi {
     }
 
     public ResponseEntity<Void> deleteContact(@ApiParam(value = "Contact id to delete",required=true) @PathVariable("contactId") Long contactId) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    	try {
+        	contactRepository.delete(contactId);
+        	return new ResponseEntity<Void>(HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<Contact> getContactById(@ApiParam(value = "ID of contact to return",required=true) @PathVariable("contactId") Long contactId) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Contact>(objectMapper.readValue("{  \"skills\" : [ {    \"level\" : \"very good\",    \"name\" : \"name\",    \"id\" : 6,    \"contacts\" : [ null, null ]  }, {    \"level\" : \"very good\",    \"name\" : \"name\",    \"id\" : 6,    \"contacts\" : [ null, null ]  } ],  \"firstName\" : \"firstName\",  \"lastName\" : \"lastName\",  \"mobilePhoneNumber\" : \"mobilePhoneNumber\",  \"address\" : \"address\",  \"fullName\" : \"fullName\",  \"id\" : 0,  \"email\" : \"email\"}", Contact.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Contact>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+    	try {
+    		Contact contact=contactRepository.findOne(contactId);
+    		if (contact==null)
+    			return new ResponseEntity<Contact>(HttpStatus.NOT_FOUND);
+    		else
+    			return new ResponseEntity<Contact>(contact,HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return new ResponseEntity<Contact>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return new ResponseEntity<Contact>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     public ResponseEntity<List<Contact>> listContacts() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Contact>>(objectMapper.readValue("[ {  \"skills\" : [ {    \"level\" : \"very good\",    \"name\" : \"name\",    \"id\" : 6,    \"contacts\" : [ null, null ]  }, {    \"level\" : \"very good\",    \"name\" : \"name\",    \"id\" : 6,    \"contacts\" : [ null, null ]  } ],  \"firstName\" : \"firstName\",  \"lastName\" : \"lastName\",  \"mobilePhoneNumber\" : \"mobilePhoneNumber\",  \"address\" : \"address\",  \"fullName\" : \"fullName\",  \"id\" : 0,  \"email\" : \"email\"}, {  \"skills\" : [ {    \"level\" : \"very good\",    \"name\" : \"name\",    \"id\" : 6,    \"contacts\" : [ null, null ]  }, {    \"level\" : \"very good\",    \"name\" : \"name\",    \"id\" : 6,    \"contacts\" : [ null, null ]  } ],  \"firstName\" : \"firstName\",  \"lastName\" : \"lastName\",  \"mobilePhoneNumber\" : \"mobilePhoneNumber\",  \"address\" : \"address\",  \"fullName\" : \"fullName\",  \"id\" : 0,  \"email\" : \"email\"} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Contact>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+    	try {
+        	return new ResponseEntity<List<Contact>>((List<Contact>)contactRepository.findAll(),HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return new ResponseEntity<List<Contact>>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return new ResponseEntity<List<Contact>>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     public ResponseEntity<Void> updateContact(@ApiParam(value = "Contact object that needs to be updated" ,required=true )  @Valid @RequestBody Contact body) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    	try {
+        	contactRepository.save(body);
+        	return new ResponseEntity<Void>(HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
